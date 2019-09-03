0ca2d7fd0f0758947fc9b2c2afa8e1c0d26db7b5
vm03 <vasy@vasy.ru>
2019-09-03 09:41:31 +0300

    out logs to file

--- .drone.sh
+++ .drone.sh
@@ -11,4 +11,4 @@ mkdir -p ~/android/lineage/.repo/local_manifests/
 curl https://raw.githubusercontent.com/vm03/android_device_yandex_Amber/lineage-16.0-ota-drone/test_manifest.xml >  ~/android/lineage/.repo/local_manifests/test.xml
 ~/bin/repo sync -c -j 16
 source build/envsetup.sh
-brunch lineage_Amber-userdebug
+brunch lineage_Amber-userdebug > build_log.txt
